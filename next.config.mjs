d255b72abca99eb03554aafbd033dd67742d7d48
Mohammed Dastager <msd116@student.bham.ac.uk>
2025-06-11 13:58:48 +0400

    Ready for deployment

---
+++ next.config.mjs
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'standalone',
+  poweredByHeader: false,
+  compress: true,
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    formats: ['image/avif', 'image/webp'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'workora.ae',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
